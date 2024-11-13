756421147648379b2cac075977525a7b314f07fb
charleykennedy <charleykennedy@ufl.edu>
2024-11-13 08:39:42 -0500

    First commit

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 def main():
     try:
@@ -8,26 +9,35 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        mole = 0, 0
         running = True
         while running:
-            draw_grid(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    clicked_x, clicked_y = event.pos
+                    if clicked_x // 32 == mole[0] and clicked_y // 32 == mole[1]:
+                        mole = (random.randrange(0, 20), random.randrange(0, 16))
+                        print(mole)
+
             screen.fill("light green")
+            draw_grid(screen)
+            screen.blit(mole_image, mole_image.get_rect(topleft= (mole[0] * 32, mole[1] * 32)))
             pygame.display.flip()
             clock.tick(60)
+
+
     finally:
         pygame.quit()
 
 #Draw 20x16 grid with 32x32 blocks
 def draw_grid(screen):
     block = 32
-    for x in range(0, 20, block):
-        for y in range(0, 16, block):
-            rect = pygame.Rect(x, y, block, block)
-            pygame.draw.rect(screen, "light green", rect, 1)
-
+    for x in range(0, 641, block):
+        for y in range(0, 513, block):
+            pygame.draw.line(screen, "black", (x,y), (x, block))
+            pygame.draw.line(screen, "black", (x,y), (block, y))
 
 if __name__ == "__main__":
     main()
